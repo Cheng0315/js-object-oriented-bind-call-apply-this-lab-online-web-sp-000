c34467532ec1a6e4700ffb16f474c981dd57fa8f
Cheng Vue <csalvue@gmail.com>
2019-02-26 14:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, args) {
   return fn.call(thisValue, args);
 }
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
